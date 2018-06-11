9ca627c7ab046f4d4bb166ecbfcb519505ab5275
鳥居隆史 <t-torii@toriitakashinoMacBook-Pro.local>
2018-06-11 23:19:21 +0900

    add delete tiles and pks clusters

---
+++ tasks/delete-tile/task.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -eu
+
+main() {
+  echo "$OPSMAN_IP $OPSMAN_DOMAIN_OR_IP_ADDRESS" >> /etc/hosts
+
+  # find tile version installed
+  echo "Retrieving current staged version of ${TILE_PRODUCT_NAME}"
+  product_version=$(om-linux \
+    --target https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
+    --client-id "${OPSMAN_CLIENT_ID}" \
+    --client-secret "${OPSMAN_CLIENT_SECRET}" \
+    --username "$OPSMAN_USERNAME" \
+    --password "$OPSMAN_PASSWORD" \
+    --skip-ssl-validation -tr  \
+    deployed-products | grep ${TILE_PRODUCT_NAME} | cut -d "|" -f 3 | tr -d " ")
+
+  echo "Deleting product [${TILE_PRODUCT_NAME}], version [${product_version}] , from ${OPSMAN_DOMAIN_OR_IP_ADDRESS}"
+
+  om-linux \
+    --target https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
+    --client-id "${OPSMAN_CLIENT_ID}" \
+    --client-secret "${OPSMAN_CLIENT_SECRET}" \
+    --username "$OPSMAN_USERNAME" \
+    --password "$OPSMAN_PASSWORD" \
+    --skip-ssl-validation -tr \
+    delete-product \
+    --product-name "$TILE_PRODUCT_NAME" \
+    --product-version "$product_version"
+
+}
+
+main
