b8c5802b2f7d95f6f49d6eb7c3b69e6eb5027cc6
鳥居隆史 <t-torii@toriitakashinoMacBook-Pro.local>
2018-06-18 14:59:54 +0900

    create-infrastructure

--- tasks/gcp/create-infrastructure/task.sh
+++ tasks/gcp/create-infrastructure/task.sh
@@ -10,22 +10,11 @@ pcf_opsman_bucket_path=$(grep -i 'us:.*.tar.gz' pivnet-opsmgr/*GCP.yml | cut -d'
 pcf_opsman_image_name=$(echo $pcf_opsman_bucket_path | sed 's%.*/\(.*\).tar.gz%opsman-\1%' | sed 's/\./-/g')
 
 
-
-if [[ ${POE_SSL_NAME1} == "" || ${POE_SSL_NAME1} == "null" ]]; then
-  echo "Generating Self Signed Certs for ${SYSTEM_DOMAIN} & ${APPS_DOMAIN} ..."
-  pcf-pipelines/scripts/gen_ssl_certs.sh "${SYSTEM_DOMAIN}" "${APPS_DOMAIN}"
-  pcf_ert_ssl_cert=$(cat ${SYSTEM_DOMAIN}.crt)
-  pcf_ert_ssl_key=$(cat ${SYSTEM_DOMAIN}.key)
-else
-  pcf_ert_ssl_cert="$POE_SSL_CERT1"
-  pcf_ert_ssl_key="$POE_SSL_KEY1"
-fi
-
 export GOOGLE_CREDENTIALS=${GCP_SERVICE_ACCOUNT_KEY}
 export GOOGLE_PROJECT=${GCP_PROJECT_ID}
 export GOOGLE_REGION=${GCP_REGION}
 
-terraform init pcf-pipelines/install-pcf/gcp/terraform
+terraform init pcf-pipelines/tasks/pks/terraform
 
 terraform plan \
   -var "gcp_proj_id=${GCP_PROJECT_ID}" \
@@ -36,39 +25,9 @@ terraform plan \
   -var "gcp_storage_bucket_location=${GCP_STORAGE_BUCKET_LOCATION}" \
   -var "prefix=${GCP_RESOURCE_PREFIX}" \
   -var "pcf_opsman_image_name=${pcf_opsman_image_name}" \
-  -var "pcf_ert_domain=${PCF_ERT_DOMAIN}" \
-  -var "system_domain=${SYSTEM_DOMAIN}" \
-  -var "apps_domain=${APPS_DOMAIN}" \
-  -var "pcf_ert_ssl_cert=${pcf_ert_ssl_cert}" \
-  -var "pcf_ert_ssl_key=${pcf_ert_ssl_key}" \
-  -var "db_app_usage_service_username=${DB_APP_USAGE_SERVICE_USERNAME}" \
-  -var "db_app_usage_service_password=${DB_APP_USAGE_SERVICE_PASSWORD}" \
-  -var "db_autoscale_username=${DB_AUTOSCALE_USERNAME}" \
-  -var "db_autoscale_password=${DB_AUTOSCALE_PASSWORD}" \
-  -var "db_diego_username=${DB_DIEGO_USERNAME}" \
-  -var "db_diego_password=${DB_DIEGO_PASSWORD}" \
-  -var "db_notifications_username=${DB_NOTIFICATIONS_USERNAME}" \
-  -var "db_notifications_password=${DB_NOTIFICATIONS_PASSWORD}" \
-  -var "db_routing_username=${DB_ROUTING_USERNAME}" \
-  -var "db_routing_password=${DB_ROUTING_PASSWORD}" \
-  -var "db_uaa_username=${DB_UAA_USERNAME}" \
-  -var "db_uaa_password=${DB_UAA_PASSWORD}" \
-  -var "db_ccdb_username=${DB_CCDB_USERNAME}" \
-  -var "db_ccdb_password=${DB_CCDB_PASSWORD}" \
-  -var "db_accountdb_username=${DB_ACCOUNTDB_USERNAME}" \
-  -var "db_accountdb_password=${DB_ACCOUNTDB_PASSWORD}" \
-  -var "db_networkpolicyserverdb_username=${DB_NETWORKPOLICYSERVERDB_USERNAME}" \
-  -var "db_networkpolicyserverdb_password=${DB_NETWORKPOLICYSERVERDB_PASSWORD}" \
-  -var "db_nfsvolumedb_username=${DB_NFSVOLUMEDB_USERNAME}" \
-  -var "db_nfsvolumedb_password=${DB_NFSVOLUMEDB_PASSWORD}" \
-  -var "db_locket_username=${DB_LOCKET_USERNAME}" \
-  -var "db_locket_password=${DB_LOCKET_PASSWORD}" \
-  -var "db_silk_username=${DB_SILK_USERNAME}" \
-  -var "db_silk_password=${DB_SILK_PASSWORD}" \
-  -var "db_cloudsqldb_tier=${DB_CLOUDSQLDB_TIER}" \
   -out terraform.tfplan \
   -state terraform-state/terraform.tfstate \
-  pcf-pipelines/install-pcf/gcp/terraform
+  pcf-pipelines/tasks/pks/terraform
 
 terraform apply \
   -state-out $root/create-infrastructure-output/terraform.tfstate \
