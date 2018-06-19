79bf63c532c5c483d086fd593f3ba4a21160b476
鳥居隆史 <t-torii@toriitakashinoMacBook-Pro.local>
2018-06-19 12:53:59 +0900

    debug

--- tasks/gcp/check-opsman-dns/task.sh
+++ tasks/gcp/check-opsman-dns/task.sh
@@ -3,6 +3,8 @@ set -e
 
 source "pcf-pipelines/functions/check_opsman_available.sh"
 
+nslookup ${OPSMAN_DOMAIN_OR_IP_ADDRESS}
+
 opsman_available=$(check_opsman_available "${OPSMAN_DOMAIN_OR_IP_ADDRESS}")
 if [[ $opsman_available != "available" ]]; then
   echo Could not reach ${OPSMAN_DOMAIN_OR_IP_ADDRESS}. Is DNS set up correctly?
