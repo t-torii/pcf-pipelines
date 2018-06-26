67a15954dba20160639e1eb8ef53adafcc95f01f
鳥居隆史 <t-torii@toriitakashinoMacBook-Pro.local>
2018-06-26 12:04:51 +0900

    upload-pas

---
+++ tasks/upload-pas/task.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -eu
+
+
+# Should the slug contain more than one product, pick only the first.
+FILE_PATH=`find ./pivnet-pas -name *.pivotal | sort | head -1`
+om-linux -t https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
+  -u "$OPS_MGR_USR" \
+  -p "$OPS_MGR_PWD" \
+  -k \
+  --request-timeout 3600 \
+  upload-product \
+  -p $FILE_PATH
