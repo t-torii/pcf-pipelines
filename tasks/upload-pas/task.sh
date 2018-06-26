55c4774efb14e5d269b7d43f2468320706c3bee5
鳥居隆史 <t-torii@toriitakashinoMacBook-Pro.local>
2018-06-26 14:07:49 +0900

    upload-pas

--- tasks/upload-pas/task.sh
+++ tasks/upload-pas/task.sh
@@ -6,6 +6,8 @@ set -eu
 # Should the slug contain more than one product, pick only the first.
 FILE_PATH=`find ./pivnet-pas -name *.pivotal | sort | head -1`
 echo $FILE_PATH
+ls -l ./pivnet-pas
+
 om-linux -t https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
   -u "$OPS_MGR_USR" \
   -p "$OPS_MGR_PWD" \
