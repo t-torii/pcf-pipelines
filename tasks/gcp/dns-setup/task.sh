01fb9432e160559f2e8c239dc5bca3db1efbfdc5
鳥居隆史 <t-torii@toriitakashinoMacBook-Pro.local>
2018-07-30 16:42:43 +0900

    dns-setup test3

--- tasks/gcp/dns-setup/task.sh
+++ tasks/gcp/dns-setup/task.sh
@@ -4,4 +4,5 @@ echo $GCP_SERVICE_KEY > /tmp/keyfile.json
 gcloud auth activate-service-account --key-file /tmp/keyfile.json
 gcloud config set project ${GCP_PROJECT_NAME}
 
-gcloud compute --project "spartan-tesla-201301" ssh --zone "asia-east1-a" "jump-server" --quiet --command "ls -l"
+gcloud compute --project "spartan-tesla-201301" ssh --zone "asia-east1-a" "jump-server" --quiet --command \
+"sudo cat /etc/resolve.conf && sudo cat /etc/dnsmasq.conf"
