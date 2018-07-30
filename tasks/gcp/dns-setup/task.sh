1c32025199eca328dee65ea2e7aadaa0c27e6258
鳥居隆史 <t-torii@toriitakashinoMacBook-Pro.local>
2018-07-30 23:59:47 +0900

    dns-setup test10

--- tasks/gcp/dns-setup/task.sh
+++ tasks/gcp/dns-setup/task.sh
@@ -27,14 +27,24 @@ gcloud config set project ${GCP_PROJECT_NAME}
 echo "==== dnsmasq.more.conf ===="
 gcloud compute --project "spartan-tesla-201301" ssh --zone "asia-east1-a" "jump-server" --quiet --command \
 "echo -e address=/opsman.pks-test.io/${pub_ip_opsman} > ./dnsmasq.more.conf"
+gcloud compute --project "spartan-tesla-201301" ssh --zone "asia-east1-a" "jump-server" --quiet --command \
+"echo -e address=/api.pks.pks-test.io/${pub_ip_pks_api} >> ./dnsmasq.more.conf"
+gcloud compute --project "spartan-tesla-201301" ssh --zone "asia-east1-a" "jump-server" --quiet --command \
+"echo -e address=/system.pks-test.io/${pub_ip_global_pcf} >> ./dnsmasq.more.conf"
+gcloud compute --project "spartan-tesla-201301" ssh --zone "asia-east1-a" "jump-server" --quiet --command \
+"echo -e address=/apps.pks-test.io/${pub_ip_global_pcf} >> ./dnsmasq.more.conf"
+gcloud compute --project "spartan-tesla-201301" ssh --zone "asia-east1-a" "jump-server" --quiet --command \
+"echo -e address=/doppler.system.pks-test.io/${pub_ip_ssh_and_doppler} >> ./dnsmasq.more.conf"
+gcloud compute --project "spartan-tesla-201301" ssh --zone "asia-east1-a" "jump-server" --quiet --command \
+"echo -e address=/loggregator.system.pks-test.io/${pub_ip_ssh_and_doppler} >> ./dnsmasq.more.conf"
 
 gcloud compute --project "spartan-tesla-201301" ssh --zone "asia-east1-a" "jump-server" --quiet --command \
 "sudo cp ./dnsmasq.more.conf /etc/dnsmasq.more.conf"
 
 echo "==== dnsmasq restart ===="
-# gcloud compute --project "spartan-tesla-201301" ssh --zone "asia-east1-a" "jump-server" --quiet --command \
-# "sudo service dnsmasq restart"
+gcloud compute --project "spartan-tesla-201301" ssh --zone "asia-east1-a" "jump-server" --quiet --command \
+"sudo service dnsmasq restart"
 
 echo "==== restore resolv.conf ===="
-# gcloud compute --project "spartan-tesla-201301" ssh --zone "asia-east1-a" "jump-server" --quiet --command \
-# "sudo cp /etc/resolv.conf.bak /etc/resolv.conf"
+gcloud compute --project "spartan-tesla-201301" ssh --zone "asia-east1-a" "jump-server" --quiet --command \
+"sudo cp /etc/resolv.conf.bak /etc/resolv.conf"
