e1f5c25fb8076a3c0c48d6f85b9a7c8a46a4d93f
鳥居隆史 <t-torii@toriitakashinoMacBook-Pro.local>
2018-07-30 22:54:49 +0900

    dns-setup test5

--- tasks/gcp/dns-setup/task.sh
+++ tasks/gcp/dns-setup/task.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
+
+cd terraform-state
+  output_json="terraform.tfstate"
+  pub_ip_global_pcf=$(echo $output_json | jq --raw-output '.pub_ip_global_pcf.value')
+  pub_ip_ssh_and_doppler=$(echo $output_json | jq --raw-output '.pub_ip_ssh_and_doppler.value')
+  pub_ip_ssh_tcp_lb=$(echo $output_json | jq --raw-output '.pub_ip_ssh_tcp_lb.value')
+  pub_ip_opsman=$(echo $output_json | jq --raw-output '.pub_ip_opsman.value')
+  pub_ip_pks_api=$(echo $output_json | jq --raw-output '.pub_ip_pks_api.value')
+cd -
+
+echo "Please configure DNS as follows:"
+echo "----------------------------------------------------------------------------------------------"
+echo "${OPSMAN_DOMAIN_OR_IP_ADDRESS} == ${pub_ip_opsman}"
+echo "api.pks.${PCF_ERT_DOMAIN} == ${pub_ip_pks_api}"
+echo "*.${SYSTEM_DOMAIN} == ${pub_ip_global_pcf}"
+echo "*.${APPS_DOMAIN} == ${pub_ip_global_pcf}"
+echo "doppler.${SYSTEM_DOMAIN} == ${pub_ip_ssh_and_doppler}"
+echo "loggregator.${SYSTEM_DOMAIN} == ${pub_ip_ssh_and_doppler}"
+echo "----------------------------------------------------------------------------------------------"
+
+
 echo "==== gcloud configuration ===="
 echo $GCP_SERVICE_KEY > /tmp/keyfile.json
 gcloud auth activate-service-account --key-file /tmp/keyfile.json
 gcloud config set project ${GCP_PROJECT_NAME}
 
+
+echo "==== dnsmasq restart ===="
 gcloud compute --project "spartan-tesla-201301" ssh --zone "asia-east1-a" "jump-server" --quiet --command \
-"sudo cat /etc/resolv.conf && sudo cat /etc/dnsmasq.conf"
+"sudo service dnsmasq restart"
+
+echo "==== restore resolv.conf ===="
 gcloud compute --project "spartan-tesla-201301" ssh --zone "asia-east1-a" "jump-server" --quiet --command \
-"pwd"
+"sudo cp /etc/resolv.conf.bak /etc/resolv.conf"
