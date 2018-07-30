2aa23bbb61b53a08b22c223db0adff32beeb6dab
鳥居隆史 <t-torii@toriitakashinoMacBook-Pro.local>
2018-07-30 16:33:02 +0900

    dns-setup

--- tasks/gcp/dns-setup/task.sh
+++ tasks/gcp/dns-setup/task.sh
@@ -4,6 +4,6 @@ echo $GCP_SERVICE_KEY > /tmp/keyfile.json
 gcloud auth activate-service-account --key-file /tmp/keyfile.json
 gcloud config set project ${GCP_PROJECT_NAME}
 
-gcloud compute --project "spartan-tesla-201301" ssh --zone "asia-east1-a" "jump-server"
+gcloud compute --project "spartan-tesla-201301" ssh --zone "asia-east1-a" "jump-server" --quiet
 ls
 exit
