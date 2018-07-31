b2fcbd376fef561a783a300c032c135945e32dac
鳥居隆史 <t-torii@toriitakashinoMacBook-Pro.local>
2018-07-31 16:15:28 +0900

    get-gcp-cost6

--- tasks/gcp/get-gcp-cost/task.sh
+++ tasks/gcp/get-gcp-cost/task.sh
@@ -4,9 +4,9 @@ echo $GCP_SERVICE_KEY > /tmp/keyfile.json
 gcloud auth activate-service-account --key-file /tmp/keyfile.json
 gcloud config set project ${GCP_PROJECT_NAME}
 
-gcloud beta pubsub subscriptions list
+gcloud beta pubsub subscriptions pull projects/spartan-tesla-201301/subscriptions/gcp-billing-sub
 
-cost_amount=$(gcloud beta pubsub subscriptions pull \
+export cost_amount=$(gcloud beta pubsub subscriptions pull \
 projects/spartan-tesla-201301/subscriptions/gcp-billing-sub --format json --quiet \
 | jq -r '.[].message.data' | base64 -d |  jq '.costAmount')
 
