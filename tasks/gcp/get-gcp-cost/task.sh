e2c97cab19a9a8bd2d6afba13c8cf067e68aaa4d
鳥居隆史 <t-torii@toriitakashinoMacBook-Pro.local>
2018-08-01 10:10:37 +0900

    get-gcp-cost7

--- tasks/gcp/get-gcp-cost/task.sh
+++ tasks/gcp/get-gcp-cost/task.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
+set -eu
+
 echo "==== gcloud configuration ===="
 echo $GCP_SERVICE_KEY > /tmp/keyfile.json
 gcloud auth activate-service-account --key-file /tmp/keyfile.json
 gcloud config set project ${GCP_PROJECT_NAME}
 
-gcloud beta pubsub subscriptions pull projects/spartan-tesla-201301/subscriptions/gcp-billing-sub
+files=$(gsutil ls "gs://${TERRAFORM_STATEFILE_BUCKET}")
 
-export cost_amount=$(gcloud beta pubsub subscriptions pull \
-projects/spartan-tesla-201301/subscriptions/gcp-billing-sub --format json --quiet \
-| jq -r '.[].message.data' | base64 -d |  jq '.costAmount')
+if [ $(echo $files | grep -c gcpbilling.json) == 0 ]; then
+  echo "{\"version\": 3}" > gcpbilling.json
+  gsutil cp gcpbilling.json "gs://${TERRAFORM_STATEFILE_BUCKET}/gcpbilling.json"
+else
+  echo "gcpbilling.json file found, skipping"
+  exit 0
+fi
 
-echo "cost = ${cost_amount}"
+billing=$(gcloud beta pubsub subscriptions pull \
+projects/spartan-tesla-201301/subscriptions/gcp-billing-sub --format json --quiet --auto-ack)
+
+cost=$(echo $billing | jq -r '.[].message.data' | base64 -d |  jq '.costAmount')
+
+echo $billing | jq '.'
+
+if [ -n $cost ]; then
+  echo $billing > gcpbilling.json
+  gsutil cp gcpbilling.json "gs://${TERRAFORM_STATEFILE_BUCKET}/gcpbilling.json"
+fi
